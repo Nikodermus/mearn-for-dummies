ba8b5b65cfed347d672d132b53923b92787aba1e
Nicolás M. Pardo <nicolasm@nikodermus.media>
2017-12-26 20:59:25 -0500

    8. Node introduction

--- routes/app.js
+++ routes/app.js
@@ -2,7 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+	res.render('index');
+});
+
+router.get('/message/:msg', function (req, res, next) {
+	res.render('new_view', {
+		message: req.params.msg || ''
+	});
+});
+
+router.post('/message', function (req, res, next) {
+	var message = req.body.message;
+	res.redirect(`/message/${message}`);
 });
 
 module.exports = router;
